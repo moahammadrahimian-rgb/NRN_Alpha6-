3dd12b2736e02781ecd3a44355096c4f48366b50
moahammad <moahammad@users.noreply.github.com>
2026-09-22 08:14:33 +0330

    connect Android app to Capacitor web core

--- android/app/src/main/java/com/nrn/alpha6/MainActivity.java
+++ android/app/src/main/java/com/nrn/alpha6/MainActivity.java
@@ -1,25 +1,6 @@
 package com.nrn.alpha6;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.graphics.Color;
-import android.view.Gravity;
-import android.widget.TextView;
+import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        TextView title = new TextView(this);
-        title.setText("NRN-ALPHA6");
-        title.setTextSize(28);
-        title.setTextColor(Color.WHITE);
-        title.setGravity(Gravity.CENTER);
-
-        title.setBackgroundColor(Color.rgb(15, 23, 42));
-
-        setContentView(title);
-    }
+public class MainActivity extends BridgeActivity {
 }
